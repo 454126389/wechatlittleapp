34a6f0c5facc70ef2f2796ee7a8039281d50c542
00500125 <454126389@qq.com>
2018-03-07 16:46:50 +0800

    Signed-off-by: 00500125 <454126389@qq.com>

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,85 @@
+// pages/welcome/welcome.js
+Page({
+
+
+  clickLogo: function () {
+    wx.reLaunch({
+      url: '../index/index'
+    });
+  },
+
+
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: 'e 生活',
+      desc: '还没进去就想分享？我就喜欢你这样的老铁☺️',
+      success: function (res) {
+        wx.showToast({
+          title: "分享成功",
+          duration: 1000,
+          icon: "success"
+        })
+      }
+    }
+  }
+})
